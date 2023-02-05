9013b6fc72f03342b6c2145f547b68ec24a8b7fd
Ehtesham289 <124323164+Ehtesham289@users.noreply.github.com>
2023-02-05 17:42:46 +0530

    Create findgreaternumber.py
    Started learning python 2 days ago and this is my first code

---
+++ findgreaternumber.py
@@ -0,0 +1,10 @@
+# Licence by Ehtesham Alam
+print("Find which number is Greater")
+number1 = int(input("Enter your First number:\n"))
+number2 = int(input("Enter your Second number:\n"))
+if(number1>number2):
+        print("Number 1 is Greater")
+elif(number1<number2):
+    print("Number 2 is Great")        
+else:
+    print("Number 2 is Greater")        
